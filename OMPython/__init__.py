20c24dc666be595608246f8d2e200e84e2a0c9bb
Ondřej Sluka <ondras12345@gmail.com>
2025-04-17 11:26:27 +0200

    Do not run omc with shell=True

--- OMPython/__init__.py
+++ OMPython/__init__.py
@@ -17,7 +17,6 @@ import os
 import platform
 import psutil
 import re
-import shlex
 import signal
 import subprocess
 import sys
@@ -188,7 +187,7 @@ class OMCSessionBase(metaclass=abc.ABCMeta):
             my_env = os.environ.copy()
             my_env["USER"] = self._currentUser
             # Because we spawned a shell, and we need to be able to kill OMC, create a new process group for this
-            self._omc_process = subprocess.Popen(self._omc_command, shell=True, stdout=self._omc_log_file, stderr=self._omc_log_file, env=my_env, preexec_fn=os.setsid)
+            self._omc_process = subprocess.Popen(self._omc_command, stdout=self._omc_log_file, stderr=self._omc_log_file, env=my_env, preexec_fn=os.setsid)
         if self._docker:
             for i in range(0, 40):
                 try:
@@ -271,12 +270,7 @@ class OMCSessionBase(metaclass=abc.ABCMeta):
         if self._interactivePort:
             extraFlags = extraFlags + ["--interactivePort=%d" % int(self._interactivePort)]
 
-        omc_path_and_args_list = omcCommand + omc_path_and_args_list + extraFlags
-
-        if sys.platform == 'win32':
-            self._omc_command = omc_path_and_args_list
-        else:
-            self._omc_command = ' '.join([shlex.quote(a) for a in omc_path_and_args_list])
+        self._omc_command = omcCommand + omc_path_and_args_list + extraFlags
 
         return self._omc_command
 
